0affac224c510b6ae7365c3496c46a23ce494851
chen <29512450@qq.com>
2017-09-13 15:37:03 +0800

    完善了章节修改功能

--- application/admin/controller/BookWriteChapter.php
+++ application/admin/controller/BookWriteChapter.php
@@ -12,4 +12,20 @@ class BookWriteChapter extends AdminController
     {
         return $this->findModel($id);
     }
+
+    function update($id)
+    {
+        $model = $this->findModel($id);
+        if(request()->param('sequence')){
+            if($model->sequence==request()->param('sequence')){
+                api(-1,"改章节已经存在！");
+            }
+        }
+        $rt = $model->allowField(true)->save(request()->param());
+        if ($rt) {
+            $this->success("更新成功！", "index");
+        } else {
+            $this->error("更新失败!");
+        }
+    }
 }
